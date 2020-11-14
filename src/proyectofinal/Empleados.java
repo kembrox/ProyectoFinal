4d300efcc3f3321cd0679f93a77adeff999759c1
XPC <XPC@DESKTOP-M3GK75E>
2020-11-14 15:37:27 -0600

    actualizacion 2.0

--- src/proyectofinal/Empleados.java
+++ src/proyectofinal/Empleados.java
@@ -1,5 +1,48 @@
 package proyectofinal;
 import javax.swing.JOptionPane; 
 public class Empleados {
+    private String nombre;
+    private int edad;
+    private String cargo;
+    private int salario;
+    
+    public Empleados(){
+        nombre="";
+        edad=0;
+        cargo="";
+        salario=0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
     
 }
